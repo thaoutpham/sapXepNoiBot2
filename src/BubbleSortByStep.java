e68a55a128688e3566625c2bb632cb20e386e042
thaoutpham <dongxuanhien65.dx@gmail.com>
2021-05-25 09:46:18 +0700

    [Thực hành] Minh hoạ thuật toán sắp xếp nổi bọt

---
+++ src/BubbleSortByStep.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class BubbleSortByStep {
+    public static void bubbleSortByStep(int[] list) {
+        boolean needNextPass = true;
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    System.out.println("Swap " + list[i] + " with " + list[i + 1]);
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter list size:");
+        int size = scanner.nextInt();
+        int[] list = new int[size];
+        System.out.println("Enter " + list.length + " values:");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = scanner.nextInt();
+        }
+        System.out.print("Your input list: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
+        System.out.println("\nBegin sort processing...");
+        bubbleSortByStep(list);
+    }
+}
